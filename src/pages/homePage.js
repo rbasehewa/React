78cfcb0d562c0ec1d5e3a72fa67a5356f944ce1b
rbasehewa <rbasehewa@gmail.com>
2022-06-06 21:50:38 +1000

    1-simple reeact blog

---
+++ src/pages/homePage.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const HomePage = () => (
+    <><h1>HI, Ryan here welcome to my Blog!</h1>
+    <p> Thanks For showing interest to check about me, I’m Ryan. This is my personal blog.
+        My blog has 12 posts in 7 categories which combinedly have 2604 words, which will take an average reader
+        ( WPM) approximately 13.02 minutes to read. The most recent post is “Drupal Installation”
+        which was published on 15 Oct 2021. The last commit was on Saturday, 16 Oct 2021 at 06:38 PM UTC.
+    </p>
+    <p> Thanks For showing interest to check about me, I’m Ryan. This is my personal blog.
+        My blog has 12 posts in 7 categories which combinedly have 2604 words, which will take an average reader
+        ( WPM) approximately 13.02 minutes to read. The most recent post is “Drupal Installation”
+        which was published on 15 Oct 2021. The last commit was on Saturday, 16 Oct 2021 at 06:38 PM UTC.
+    </p>
+    <p> Thanks For showing interest to check about me, I’m Ryan. This is my personal blog.
+        My blog has 12 posts in 7 categories which combinedly have 2604 words, which will take an average reader
+        ( WPM) approximately 13.02 minutes to read. The most recent post is “Drupal Installation”
+        which was published on 15 Oct 2021. The last commit was on Saturday, 16 Oct 2021 at 06:38 PM UTC.
+    </p>
+    <p> Thanks For showing interest to check about me, I’m Ryan. This is my personal blog.
+        My blog has 12 posts in 7 categories which combinedly have 2604 words, which will take an average reader
+        ( WPM) approximately 13.02 minutes to read. The most recent post is “Drupal Installation”
+        which was published on 15 Oct 2021. The last commit was on Saturday, 16 Oct 2021 at 06:38 PM UTC.
+    </p>
+    <p> Thanks For showing interest to check about me, I’m Ryan. This is my personal blog.
+        My blog has 12 posts in 7 categories which combinedly have 2604 words, which will take an average reader
+        ( WPM) approximately 13.02 minutes to read. The most recent post is “Drupal Installation”
+        which was published on 15 Oct 2021. The last commit was on Saturday, 16 Oct 2021 at 06:38 PM UTC.
+    </p>
+    <p> Thanks For showing interest to check about me, I’m Ryan. This is my personal blog.
+        My blog has 12 posts in 7 categories which combinedly have 2604 words, which will take an average reader
+        ( WPM) approximately 13.02 minutes to read. The most recent post is “Drupal Installation”
+        which was published on 15 Oct 2021. The last commit was on Saturday, 16 Oct 2021 at 06:38 PM UTC.
+    </p>
+    </>
+);
+
+export default HomePage;
